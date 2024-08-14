92b9d4cf74fa43d6d9ed92b6f5746a5eee10815d
Faber92 <dogger56@hotmail.it>
2024-08-14 15:36:37 +0200

    Add Subito custom reporter

--- Source/SwiftLintCore/Models/ReportersList.swift
+++ Source/SwiftLintCore/Models/ReportersList.swift
@@ -16,6 +16,7 @@ public let reportersList: [any Reporter.Type] = [
     RelativePathReporter.self,
     SARIFReporter.self,
     SonarQubeReporter.self,
+    SubitoReporter.self,
     SummaryReporter.self,
     XcodeReporter.self,
 ]
